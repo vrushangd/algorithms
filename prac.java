1461516383e4ef51d5625de0229ffe2c119b8008
vrushang <vrushangapple@gmail.com>
2018-11-23 01:11:04 +0530

    stucked

--- prac.java
+++ prac.java
@@ -1,7 +1,9 @@
-class bubblesort{
+class prac
+{
     public static void main(String[]args){
-        int[] a = new int[5]
-        int n=a.length();
+        int[] a = new int[]{1,3,2,6,4,7};
+        int temp=0;
+        int n=a.length;
         for(int k=1;k<n-1;k++){
             for(int i=0;i<n-2;i++){
                 if(a[i]>a[i+1]){
